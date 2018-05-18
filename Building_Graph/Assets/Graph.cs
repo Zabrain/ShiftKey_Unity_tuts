3cd6a9bc9b4278bab29e8e675312c735c21b23b6
Chinenye Ndulue <nduluechinenye@gmail.com>
2018-05-18 17:07:53 -0300

    Started the Graphs tutorial

---
+++ Building_Graph/Assets/Graph.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+
+    public Transform pointPrefab;
+
+    private void Awake()
+    {
+        //shows at the prefab's default point
+        //Instantiate(pointPrefab);
+
+        ////store the point od the prefab
+        //Transform point = Instantiate(pointPrefab);
+        ////assign the prefab to a vector variable
+        //point.localPosition = Vector3.right;
+
+        //int i = 0;
+        //while (i++ < 10)
+        //{
+        //    Transform point = Instantiate(pointPrefab);
+        //    point.localPosition = Vector3.right * i;
+
+        //}
+
+        for (int i=0; i<10; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            point.localPosition = Vector3.right *(( i+0.5f)/5f-1f);
+            //scale cubes in 2 units domain
+            point.localScale = Vector3.one / 5f;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
